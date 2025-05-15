ddb8a1d1bd08ea10368367d0839afa4d386d8c0a
Jim Madge <jmadge@turing.ac.uk>
2025-05-15 11:52:14 +0100

    Add test for exception

--- tests/test_inject.py
+++ tests/test_inject.py
@@ -1,5 +1,7 @@
 from all_all_contributors.inject import inject
 
+from pytest import raises
+
 
 class TestInject:
     def test_inject(self, target_all_contributors_rc_object):
@@ -8,3 +10,8 @@ class TestInject:
         assert isinstance(all_contributors_rc, dict)
         assert "contributors" in all_contributors_rc.keys()
         assert all_contributors_rc.get("contributors") == contributors
+
+    def test_inject_no_contributors(self):
+        contributors = ["hello"]
+        with raises(AttributeError):
+            inject({}, contributors)
