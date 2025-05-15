3652f2f1ca856146fdb0e06a9cef7f6e2daf9e19
Jim Madge <jmadge@turing.ac.uk>
2025-05-15 11:50:42 +0100

    Correct isisnstance assertion

--- tests/test_inject.py
+++ tests/test_inject.py
@@ -5,6 +5,6 @@ class TestInject:
     def test_inject(self, target_all_contributors_rc_object):
         contributors = ["hello"]
         all_contributors_rc = inject(target_all_contributors_rc_object, contributors)
-        assert isinstance(dict, all_contributors_rc)
+        assert isinstance(all_contributors_rc, dict)
         assert "contributors" in all_contributors_rc.keys()
         assert all_contributors_rc.get("contributors") == contributors
