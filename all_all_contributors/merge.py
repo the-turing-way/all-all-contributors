9e6887a0ebefe5dc8006ccdbaad16b4ff3c1827c
Jim Madge <jmadge@turing.ac.uk>
2025-05-20 16:23:06 +0100

    Avoid magic string

--- all_all_contributors/merge.py
+++ all_all_contributors/merge.py
@@ -2,6 +2,8 @@
 
 from typing import List, Dict, Any
 
+_unique_key = "profile"
+
 
 def merge_contributors(
     contributors_list: List[Dict[str, Any]]
@@ -22,20 +24,20 @@ def merge_contributors(
             each contributor appears only once with their combined contributions.
 
     Note:
-        The function merges contributors based on their profile URL and
-        aggregates contributions for each unique profile.
+        The function merges contributors based on merge._unique_key and
+        aggregates contributions types.
     """
 
     unique_profiles = []
     merged_contributors = []
 
     for contributor in contributors_list:
-        if contributor["profile"] not in unique_profiles:
-            unique_profiles.append(contributor["profile"])
+        if contributor.get(_unique_key) not in unique_profiles:
+            unique_profiles.append(contributor.get(_unique_key))
             merged_contributors.append(contributor)
         else:
             # find the index of the contributor in the list
-            index = unique_profiles.index(contributor["profile"])
+            index = unique_profiles.index(contributor.get(_unique_key))
             merged_contributors[index]["contributions"].extend(
                 contributor["contributions"]
             )
